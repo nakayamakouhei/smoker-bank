22f161f6fe7aa71ab01e7d9384ae6e1a50f2ae35
中山航兵 <nakayamakouhei@kouhei.local>
2025-10-06 15:58:20 +0900

    feat(history):履歴機能の追加

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,8 +2,39 @@ class HistoriesController < ApplicationController
   def index
     smokes = current_user.smokes.includes(:cigarette)
     custom_logs = current_user.custom_cigarette_logs.includes(:custom_cigarette)
-  
-    # created_at で新しい順に並べる
-    @histories = (smokes + custom_logs).sort_by(&:created_at).reverse
-  end  
+    @histories = smokes + custom_logs
+
+    # 並び替え
+    @histories =
+      case params[:sort]
+      when 'date_asc'
+        @histories.sort_by(&:bought_date)
+      when 'name_asc'
+        @histories.sort_by { |log| log.is_a?(Smoke) ? log.cigarette.name : log.custom_cigarette.name }
+      when 'price_desc'
+        @histories.sort_by { |log| log.is_a?(Smoke) ? log.cigarette.price : log.custom_cigarette.price }.reverse
+      when 'price_asc'
+        @histories.sort_by { |log| log.is_a?(Smoke) ? log.cigarette.price : log.custom_cigarette.price }
+      else
+        @histories.sort_by(&:bought_date).reverse
+      end
+
+    # 日付フィルタ
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @histories.select! { |h| h.bought_date.between?(start_date, end_date) }
+    end
+
+    # 合計
+    @total_amount = @histories.sum do |log|
+      log.is_a?(Smoke) ? log.cigarette.price * log.packs : log.custom_cigarette.price * log.packs
+    end
+    @total_packs = @histories.sum(&:packs)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
 end
