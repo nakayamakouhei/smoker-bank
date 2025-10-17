daf7c1ea8bbac698678ea3f9935569d989e64168
中山航兵 <nakayamakouhei@kouhei.local>
2025-10-17 14:49:33 +0900

    chore(database.yml):DB名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_16_030744) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_17_053934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,13 +43,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_16_030744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_custom_cigarettes_on_user_id"
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "smokes", force: :cascade do |t|
