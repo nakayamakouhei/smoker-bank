88a0c2f56382638af2c54273cf90c7c2d97ce867
中山航兵 <nakayamakouhei@kouhei.local>
2025-10-19 13:26:47 +0900

    fix(contact):問い合わせメール送信の設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587,
+    port:                 465,
     domain:               "gmail.com",
     user_name:            ENV["MAIL_USER"],
     password:             ENV["MAIL_PASSWORD"],
     authentication:       "plain",
+    ssl:                  true,
     enable_starttls_auto: true
   }
 
