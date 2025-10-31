b1681aaf15014fcd8b0ee254bdf93e329c3053e2
中山航兵 <nakayamakouhei@kouhei.local>
2025-10-31 22:14:03 +0900

    feat(select):alert設定

--- app/controllers/cigarettes_controller.rb
+++ app/controllers/cigarettes_controller.rb
@@ -7,10 +7,12 @@ class CigarettesController < ApplicationController
 
   # routes.rbで呼び出し
   def update_selection
-    if current_user.update(current_cigarette_id: params[:current_cigarette_id])
+    if params[:current_cigarette_id].blank?
+      redirect_to select_cigarettes_path, alert: "銘柄を選択してください"
+    elsif current_user.update(current_cigarette_id: params[:current_cigarette_id])
       redirect_to authenticated_root_path, notice: "銘柄を変更しました"
     else
-      redirect_to cigarettes_select_path, alert: "銘柄を変更できませんでした"
+      redirect_to select_cigarettes_path, alert: "銘柄を変更できませんでした"
     end
   end
 end
