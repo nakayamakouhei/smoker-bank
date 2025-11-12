fd7fff7c7eb9869f06408dbdd5db02ed13f5dd1a
中山航兵 <nakayamakouhei@kouhei.local>
2025-11-12 16:45:59 +0900

    fix:rakeタスクが重複で呼び出されないよう修正

--- app/controllers/internal/cron_controller.rb
+++ app/controllers/internal/cron_controller.rb
@@ -1,6 +1,7 @@
 require "rake"
 
 class Internal::CronController < ActionController::API
+  before_action :load_rake_tasks_once
 
   def run
     token = params[:token].to_s
@@ -8,11 +9,18 @@ class Internal::CronController < ActionController::API
       return head :unauthorized
     end
 
-    Rails.application.load_tasks
-
+    # Rakeタスクを1回だけ読み込んだ上で、呼び出す
     Rake::Task["notifications:send_daily"].reenable
     Rake::Task["notifications:send_daily"].invoke
 
     head :ok
+  end
+
+  private
+
+  def load_rake_tasks_once
+    return if defined?(@@tasks_loaded) && @@tasks_loaded
+    Rails.application.load_tasks
+    @@tasks_loaded = true
   end
 end
