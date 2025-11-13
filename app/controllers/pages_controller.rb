31f5a1e7a91c13dc6794113209d858fa6d19b526
中山航兵 <nakayamakouhei@kouhei.local>
2025-11-13 21:53:03 +0900

    chore(contact):バリデーション追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,34 @@ class PagesController < ApplicationController
 
   def terms; end
   def privacy; end
-  def contact; end
+  def contact
+    @contact_form = ContactForm.new
+  end
 
   def send_contact
-    name = params[:name]
-    email = params[:email]
-    message = params[:message]
+    @contact_form = ContactForm.new(contact_form_params)
+
+    unless @contact_form.valid?
+      flash.now[:alert] = @contact_form.errors.full_messages.join("、")
+      return render :contact, status: :unprocessable_entity
+    end
 
     # SendGrid経由で送信
-    SendgridMailer.contact_admin(name, email, message)
-    SendgridMailer.contact_user(name, email)
+    SendgridMailer.contact_admin(@contact_form.name, @contact_form.email, @contact_form.message)
+    SendgridMailer.contact_user(@contact_form.name, @contact_form.email)
 
+    flash[:notice] = "お問い合わせを送信しました。"
     redirect_to contact_complete_path
   end
 
   def contact_complete; end
+
+  private
+
+  def contact_form_params
+    params.require(:contact_form)
+          .permit(:name, :email, :message)
+  rescue ActionController::ParameterMissing
+    {}
+  end
 end
